bb1072e81b1137e28d3efab6fc76081b0c8907af
Jasen Wright <jasen.g.wright@gmail.com>
2017-03-22 19:53:09 -0700

    Animation and movement

---
+++ xerogGame/Assets/Scripts/CharacterControllerScripts.cs
@@ -0,0 +1,63 @@
+﻿/*
+ / Character controller. Animates the player as they move and jump.
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerScripts : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+	bool facingRight = true;
+
+	Animator anim;
+
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.01f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 15;
+
+    public float maxJump = 30;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool ("Ground", grounded);
+
+		anim.SetFloat ("vSpeed", GetComponent<Rigidbody2D> ().velocity.y);
+
+		float move = Input.GetAxis ("Horizontal");
+
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+
+		if (move > 0 && !facingRight)
+			Flip ();
+		else if (move < 0 && facingRight)
+			Flip ();
+	}
+
+	void Update () {
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			anim.SetBool ("Ground", false);
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, jumpForce));
+            GetComponent<Rigidbody2D>().velocity = Vector3.ClampMagnitude(GetComponent<Rigidbody2D>().velocity, maxJump);
+        }
+	}
+
+	void Flip() {
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
