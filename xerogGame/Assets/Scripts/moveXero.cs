d5edd02d5af3a4e8e27d7dde5f91080e3fcf9faf
Conner <connerleverett@gmail.com>
2017-03-24 08:40:23 -0700

    Started Intro

---
+++ xerogGame/Assets/Scripts/moveXero.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveXero : MonoBehaviour {
+
+    public GameObject xero;
+    bool changeColor = false;
+    public GameObject startPosition;
+    public GameObject endPosition;
+    public float speed = 5.0F;
+    private float startTime;
+    private float journeyLength;
+    public AudioClip whooshSound;
+
+    // Use this for initialization
+    void Start () {
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startPosition.transform.position, endPosition.transform.position);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        xero.transform.position = Vector3.Lerp(startPosition.transform.position, endPosition.transform.position, fracJourney);
+
+        SpriteRenderer renderer = xero.GetComponent<SpriteRenderer>();
+        Color tempColor = renderer.color;
+        
+
+        if (xero.transform.position == endPosition.transform.position && changeColor==false)
+        {
+            //renderer.material.SetColor("_Color", Color.white);
+            //renderer.color = new Color(0f, 0f, 0f, 1f); // Set to opaque black
+            changeColor = true;
+        }
+
+        //renderer.color = tempColor;
+        
+    }
+
+
+}
