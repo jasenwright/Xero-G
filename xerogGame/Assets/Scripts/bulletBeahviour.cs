75a791c48404434fe23674e2f2bc1c89226b6e74
Conner <connerleverett@gmail.com>
2017-04-09 18:44:32 -0700

    Addded health bar, tomorrow will be functionality

--- xerogGame/Assets/Scripts/bulletBeahviour.cs
+++ xerogGame/Assets/Scripts/bulletBeahviour.cs
@@ -7,7 +7,14 @@ public class bulletBeahviour : MonoBehaviour {
     //If the player hits a star, the star is destroyed and the score increases
     void OnTriggerEnter2D(Collider2D col) {
         if (col.tag == "Player" || col.tag=="tile") {
+   
+            //Destroy bullet
             Destroy(gameObject);
+        //Player takes damage when hit
+        if (col.tag == "Player") {
+            playerHealth player = col.GetComponent<playerHealth>();
+            player.takeDamage(10);
+        }
         }
     }
 }
---
+++ xerogGame/Assets/Scripts/playerHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerHealth : MonoBehaviour {
+
+    public float startingHealth = 100f;
+    public float currentHealth;
+
+    // Use this for initialization
+    void Start () {
+        currentHealth = startingHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void takeDamage(float amount) {
+        Debug.Log("entered takeDamage");
+        currentHealth -= amount;
+    }
+}
